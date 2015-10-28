c3c2a085b209b704175678439d5bc90282eae147
SithLordDarthVader <mattomanka@outlook.com>
2015-10-28 03:23:35 +0200

    Work with factories and ngresource.

---
+++ www/js/factories.js
@@ -0,0 +1,13 @@
+angular.module('starter.factories', ['ngResource'])
+	
+	.factory('ToursPost', function($resource) {
+		return $resource('http://gid.areyoualive.ru/api/tours.php');
+	})
+
+	.factory('LocationsPost', function($resource) {
+		return $resource('http://gid.areyoualive.ru/api/locations.php');
+	})
+
+	.factory('InnerLocationsPost', function($resource) {
+		return $resource('http://gid.areyoualive.ru/api/inner_locations.php');
+	});
