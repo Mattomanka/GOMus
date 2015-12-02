842804fc333b685b387eb446289f4efdc42a6a4b
kadmaxim <max.kadatsky@gmail.com>
2015-12-02 03:15:34 +0200

    Add parser for tested string.

--- www/js/controllers/QuestCtrl.js
+++ www/js/controllers/QuestCtrl.js
@@ -1,13 +1,44 @@
-angular.module('starter.controllers').controller('QuestCtrl', ['$scope', '$ionicModal', '$http', '$stateParams', '$ionicLoading', function ($scope, $ionicModal, $http, $stateParams, $ionicLoading) {
+angular.module('starter.controllers').controller('QuestCtrl', ['$scope', '$ionicModal', '$sce','$http', '$stateParams', '$ionicLoading', function ($scope, $ionicModal, $sce, $http, $stateParams, $ionicLoading) {
   	
   console.log($stateParams);
 
+  var testSTR = "Hello my name is {*Max, Ann,Yan}! How are you, {brother,*pal,fellow}?";
 
   $scope.showSelectValues = function(mySelect) {
     console.log(mySelect);
   }
-
-
+	function qmaxParser(str){
+			str = str.substr(1,str.length-2); // Separate { }
+			var variants = str.split(','); // Separate each variant
+			
+			var currentID = Math.round((Math.random(2)*20)); //Generated rand model ID
+			var output = '<label class="item item-input item-select" ng-class="qmax'+currentID+'"> 	\
+	                    <div class="input-label">	\
+	                        &nbsp;	\
+	                    </div>	\
+	                    <select ng-model="qmax'+currentID+'" > \
+						';
+						
+			for(var i = 0; i < variants.length; i++){
+				var currentVariant = variants[i].trim();
+				var currentValue = false;
+				if(currentVariant[0]=='*'){ 
+					currentValue = true; 
+					currentVariant = currentVariant.substr(1);// remove *
+				}else {
+					currentValue = false;
+				}
+				
+				output += '<option value="'+currentValue+'">'+currentVariant+'</option>';	
+					
+			}
+			 output += '</select>	\
+	                </label>';
+		return output;
+	}
+	
+  $scope.CurrenHTML = $sce.trustAsHtml(testSTR.replace(/[{]([*]{0,}[\w]+[,]{0,}[ ]{0,})*[}]/g,qmaxParser));
+  
   $scope.questions = [
     { 'question': 'Text question', 
       'answers': ['first', 'second', 'third', 'fourth'], 
@@ -29,12 +60,12 @@ angular.module('starter.controllers').controller('QuestCtrl', ['$scope', '$ionic
 
 
 
-  $ionicModal.fromTemplateUrl('image-quest-modal.html', {
-    scope: $scope,
-    animation: 'slide-in-up'
-  }).then(function(modal) {
-    $scope.modal = modal;
-  });
+  // $ionicModal.fromTemplateUrl('image-quest-modal.html', {
+    // scope: $scope,
+    // animation: 'slide-in-up'
+  // }).then(function(modal) {
+    // $scope.modal = modal;
+  // });
 
   $scope.submitAnswer=function(){
     console.log($scope.questions);
